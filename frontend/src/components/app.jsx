08a775d075338df306249d115ab85c8bf7fbebd7
Kevin Bastoul <kaycbas@gmail.com>
2021-03-01 15:24:29 -0800

    initial frontend setup

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Switch } from 'react-router-dom';
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+// import Landing from './landing/landing';
+// import ArticleShowContainer from './articles/article_show_container';
+// import MainContainer from './main/main_container';
+
+const App = () => (
+    <div>Hello, World!</div>
+);
+
+export default App;
