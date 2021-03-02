cb4c53a766e8ba8b8705f14c50f2565c439e3615
Kevin Bastoul <kaycbas@gmail.com>
2021-03-01 16:13:02 -0800

    continue frontend auth setup

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>An App Skeleton</h1>
+        <footer>
+          Copyright &copy; 2021 KB
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
-import entities from './entities/entities_reducer';
-// import session from './session/session_reducer';
+import entities from './entities_reducer';
+import session from './session_reducer';
 // import errors from './errors/errors_reducer';
 // import ui from './ui/ui_reducer';
 
 export default combineReducers({
     entities,
-    // session,
+    session,
     // errors,
     // ui
 })
