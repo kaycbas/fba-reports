4f85ad5a5e81baf343399048d30cdfff61ddfabc
Kevin Bastoul <kaycbas@gmail.com>
2021-03-03 14:23:25 -0800

    add admin login

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -7,12 +7,14 @@ import NavbarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import SigninFormContainer from './session/signin_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import AdminFormContainer from './session/admin_form_container';
 
 const App = () => (
   <div>
     <Switch>
         <AuthRoute exact path="/signin" component={SigninFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
+        <AuthRoute exact path="/admin" component={AdminFormContainer} />
         
         <ProtectedRoute path="/" component={NavbarContainer} />
         <ProtectedRoute exact path="/" component={MainPage} />
---
+++ frontend/src/components/session/admin_form_container.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { login, clearSessionErrors } from '../../actions/session_actions';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+function SigninForm ({ currentUser, login, errors, clearErrors }) {
+    const email = 'admin@gmail.com';
+    // ?x3FE)#t+wRk
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        clearErrors();
+    }, [clearErrors]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let user = {
+            email,
+            password
+        };
+        login(user);
+        // console.log('submitted login');
+    }
+
+    const renderErrors = () => {
+        if (!errors || errors.length === 0) return null;
+        console.log(`Errors: ${Object.values(errors)}`);
+        return (
+            <ul className="session-errors">
+                {Object.values(errors).map((err, i) => (
+                    <li key={`error-${i}`}>{err}</li>
+                ))}
+            </ul>
+        );
+    }
+
+    return (
+        <div className="session-wrapper">
+            <form className="session-form" onSubmit={handleSubmit}>
+                <h3 className='session-header'>Admin Sign In</h3>
+                {renderErrors()}
+                <input type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                />
+                <div className="submit-btn">
+                    <input className="submit-btn" type="submit" value="Submit" />
+                </div>
+                {/* <Link className="session-switch-link" to="/signup">No account? Sign up &gt;</Link> */}
+            </form>
+        </div>
+    )
+}
+
+const mapStateToProps = (state, _ownProps) => ({
+  currentUser: state.session.user,
+  errors: state.errors.session,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  login: (user) => dispatch(login(user)),
+  clearErrors: () => dispatch(clearSessionErrors()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SigninForm);
