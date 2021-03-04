096806d718d97c75c03656b4cf5b48e0e0b00be3
Kevin Bastoul <kaycbas@gmail.com>
2021-03-03 16:28:19 -0800

    add some admin dashboard

--- frontend/src/components/admin/dashboard_container.js
+++ frontend/src/components/admin/dashboard_container.js
@@ -4,25 +4,34 @@ import { connect } from 'react-redux';
 // import { logout } from '../../actions/session_actions';
 // import { Link } from 'react-router-dom';
 
-function Dashboard({ fetchUsers }) {
+function Dashboard({ fetchUsers, users }) {
     useEffect(() => {
-        fetchUsers();
-    });
+        if (users.length === 0) fetchUsers();
+    }, [users]);
+
+    const renderUsers = () => {
+        if (users.length === 0) return null;
+        return users.map(user => (
+            <div className="user-list-item">{user.email}</div>
+        ));
+    }
 
     return (
         <div className='admin-dashboard-wrapper'>
-            <h1>Admin Dashboard</h1>
+            <h1 className="admin-dashboard-header">Admin Dashboard</h1>
+            <div className="admin-dashboard-subheader">Connected Users:</div>
+            {renderUsers()}
         </div>
     )
 }
 
-// const mapStateToProps = (state, _ownProps) => ({
-
-// });
+const mapStateToProps = (state, _ownProps) => ({
+    users: Object.values(state.entities.users),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   fetchUsers: () => dispatch(fetchUsers()),
 });
 
-export default connect(null, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
 // export default Dashboard;
