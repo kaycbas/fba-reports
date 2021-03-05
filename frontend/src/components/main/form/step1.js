9ce9bf523028f75e24338a41c7f308dc47a3af99
Kevin Bastoul <kaycbas@gmail.com>
2021-03-04 18:51:37 -0800

    style instructions

--- frontend/src/components/main/form/step1.js
+++ frontend/src/components/main/form/step1.js
@@ -4,11 +4,11 @@ export default function Step1({ currentStep, next, prev }) {
     if (currentStep !== 1) return null;
     return (
         <div className="step-wrapper">
-            <div>Step 1</div>
+            <div className="step-header">Step 1</div>
             <div className="connection-instructions">
-                <li>1. Log into your Seller Central account as the primary account holder.</li>
-                <li>2. Click the 'Authenticate New Developer' button and follow the authorization workflow.</li>
-                <li>3. When prompted, enter the below Developer ID.</li>
+                <li>Log into your Seller Central account as the primary account holder.</li>
+                <li>Click the 'Authenticate New Developer' button and follow the authorization workflow.</li>
+                <li>When prompted, enter the below Developer ID.</li>
                 <div className="step-dev-id">HFJK543KJL5</div>
             </div>
             <div className="step-ctrls">
