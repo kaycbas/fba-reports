7883bccf6b532a6c148bc6f818ffc93908f88f0b
Kevin Bastoul <kaycbas@gmail.com>
2021-03-05 15:19:02 -0800

    add minor fixes

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -9,8 +9,8 @@ function MainPage() {
     return (
       <div className="welcome-wrapper">
         <div className="welcome-header">Welcome</div>
-        <div className="welcome-message">This process is designed to automate the building of your Amazon FBA P&L.</div>
-        <div className="welcome-message">Please follow the instructions to connect your Seller Central account and provide the necessary ASIN and expenses information.</div>
+        <div className="welcome-message">This app is designed to automate the building of your Amazon FBA P&L.</div>
+        <div className="welcome-message">Please follow the instructions to connect your Seller Central account and provide the necessary ASIN and expense information.</div>
         <div onClick={() => setStarted(true)} className="welcome-begin-btn">Click to begin</div>
       </div>
     )
