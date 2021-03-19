ee73dfa5b45722075870d9c9b2e2579029086134
Kevin Bastoul <kaycbas@gmail.com>
2021-03-19 14:18:08 -0700

    tweak clg

--- routes/api/authorize.js
+++ routes/api/authorize.js
@@ -12,13 +12,14 @@ const passport = require('passport');
 // amazon_callback_uri, amazon_state, selling_partner_id
 //
 // Example req
-// https://localhost:5000/api/authorize?amazon_callback_uri=https://amazon.com/apps/authorize/confirm/amzn1.sellerapps.app.2eca283f-9f5a-4d13-b16c-474EXAMPLE57&amazon_state=amazonstateexample&selling_partner_id=A3FHEXAMPLEYWS
+// localhost:5000/api/authorize?amazon_callback_uri=https://amazon.com/apps/authorize/confirm/amzn1.sellerapps.app.2eca283f-9f5a-4d13-b16c-474EXAMPLE57&amazon_state=amazonstateexample&selling_partner_id=A3FHEXAMPLEYWS
+// https://imperialflippers.com/api/authorize?amazon_callback_uri=https://amazon.com/apps/authorize/confirm/amzn1.sellerapps.app.2eca283f-9f5a-4d13-b16c-474EXAMPLE57&amazon_state=amazonstateexample&selling_partner_id=A3FHEXAMPLEYWS
 router.get('/', (req, res) => {
     let amazon_callback_uri = req.query.amazon_callback_uri;
     let amazon_state = req.query.amazon_state;
     let selling_partner_id = req.query.selling_partner_id;
 
-    console.log(`amazon_callback_uri: ${amazon_callback_uri}, amazon_state: ${amazon_state}, selling_partner_id: ${selling_partner_id}`)
+    console.log(`PARAMETERS IN AUTHORIZE ROUTE: amazon_callback_uri: ${amazon_callback_uri}, amazon_state: ${amazon_state}, selling_partner_id: ${selling_partner_id}`)
 
     res.json({ amazon_callback_uri, amazon_state, selling_partner_id });
 })
